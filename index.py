ad0ae9cc1a041983efed8f9d381c817fb805428d
Marc <phyrenight@gmail.com>
2016-05-06 05:42:37 -0400

    feat: display all warehouses
    Will be need for user to know what warehouse are available.
    It will also help in making menu choices, such as deleting,
     renaming and adding product to certain warehouses.

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 import product
-import inventory
+from inventory import Inventory
 
 def addWarehouse(warehouses):
     """ 
@@ -7,19 +7,35 @@ def addWarehouse(warehouses):
 
         function: gets data for the warehouse from the user.
 
-        returns: a dictionary
+        returns: none
     """
     storage = {}
     print "Enter a name for the warehouse:"
     storage['name'] = raw_input("> ")
     print "Enter the max capacity of the warehouse:"
     storage['capacity'] = int(raw_input("> "))
-    warehouses.append(inventory.Inventory(storage['name'], storage['capacity']))
+    warehouses.append(Inventory(storage['name'], storage['capacity']))
 
-def main():
+
+def showAllWarehouses(warehouses):
+    """
+        args: warehouses stores all current warehouses
+        function: prints name and capacity for each warehouse
+    """
+    if len(warehouses) < 1:
+        print "There are no warehouses to show."
+    else:
+        for i in warehouses:
+            print "Name: {} capacity: {}".format(i.name, i.capacity)
+
+
+
+def test():
     warehouses = []
+    showAllWarehouses(warehouses)
     addWarehouse(warehouses)
-    print warehouses[0].name
-    print warehouses[0].capacity 
+    warehouses.append(Inventory('two',90))
+    showAllWarehouses(warehouses)
 
-main()
+test()
+
